db34b8f62f63890258fbf6cf1b80e6cd6e2b25e0
Brett Bilyk <brettbilyk@gmail.com>
2022-02-09 15:58:52 -0700

    Add custom background textures

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'margin-bg-pattern': "url('/src/images/bright-squares.png')",
+        'main-bg-pattern': "url('./src/images/tweed.png')"
+      }
+    },
   },
   plugins: [],
 }
