f6c257ab5ede558734ed1ffda4d7a393a604e59e
Brett Bilyk <brettbilyk@gmail.com>
2022-02-08 20:36:00 -0700

    Move CssBaseLine, move and comment out ThemeProvider

--- src/App.js
+++ src/App.js
@@ -3,27 +3,20 @@ import Home from './components/Home'
 import Nav from './components/Nav';
 import ShowMedia from './components/ShowMedia/ShowMedia'
 import Banner from './components/Banner'
-import { CssBaseline } from '@mui/material';
 import { Routes, Route, Link } from "react-router-dom";
 import React from 'react';
-import theme from './theme'
-import { ThemeProvider } from '@mui/private-theming';
 
 function App() {
   return (
     <React.Fragment>
-      <CssBaseline>
-        <ThemeProvider theme={theme}>
-          <div className="App">
-            <Nav />
-            <Banner />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/showmedia" element={<ShowMedia />} />
-            </Routes>
-          </div>
-        </ThemeProvider>
-      </CssBaseline>
+      <div className="App">
+        <Nav />
+        <Banner />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/showmedia" element={<ShowMedia />} />
+        </Routes>
+      </div>
     </React.Fragment>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { ThemeProvider } from '@mui/private-theming';
+import { CssBaseline } from '@mui/material';
+import theme from './theme'
+
+
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    {/* <ThemeProvider theme={theme}> */}
+      <CssBaseline>
+        <App />
+      </CssBaseline>
+    {/* </ThemeProvider> */}
   </BrowserRouter>,
   document.getElementById("root")
 );
