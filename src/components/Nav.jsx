fba562e8873722e702fb01f91657d695288c066c
rewpt <zpermack@gmail.com>
2022-02-09 18:57:01 -0400

    add first nav bar completion with responsive feature

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,11 +2,10 @@ import * as React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBell, faUserPlus, faCircleUser, faBars } from '@fortawesome/free-solid-svg-icons'
 
-
-const navItemR = "md:my-0 my-7 md:ml-8 cursor-pointer text-2xl hover:text-gray-400 duration-300"
-const hamburger = "text-2xl absolute right-8 top-6 cursor-pointer md:hidden"
-
 export default function Nav() {
+  const navItemR = "md:my-0 my-7 md:ml-8 cursor-pointer text-2xl hover:text-gray-400 duration-300"
+  const hamburger = "text-2xl absolute right-8 top-6 cursor-pointer md:hidden"
+  let [open, setOpen] = React.useState(false);
   return (
     <div className='shadow-md w-full fixed top-0 left-0 '>
       <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
@@ -16,12 +15,12 @@ export default function Nav() {
           </span>
           Catchlight
         </div>
-        <div>
-        <FontAwesomeIcon className={hamburger}icon={faBars} />
+        <div onClick={()=>setOpen(!open)} className=''>
+        <FontAwesomeIcon name={open ? 'close' : 'menu'} className={hamburger}icon={faBars} />
         </div>
-        <ul className='md:flex md:items-center md:pb-0 pb-12 absolute 
+        <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute 
         md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto
-        md:pl-0 pl-9 transition-all duration-400 ease-in'>
+        md:pl-0 pl-9 transition-all duration-400 ease-in ${open ? 'top-20 opacity-100' : 'top-[-490px]'} md:opacity-100 opacity-80`}>
           <li><FontAwesomeIcon className={navItemR}  icon={faBell} /></li>
           <li><FontAwesomeIcon className={navItemR} icon={faUserPlus} /></li>
           <li><FontAwesomeIcon className={navItemR} icon={faCircleUser} /></li>
