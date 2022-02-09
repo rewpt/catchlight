b4a6bd78f4a9c91cecdb0c6202a99b0cb0b92a9f
Brett Bilyk <brettbilyk@gmail.com>
2022-02-09 15:58:36 -0700

    Add margin and main comp

--- src/App.js
+++ src/App.js
@@ -4,26 +4,28 @@ import Nav from './components/Nav';
 import ShowMedia from './components/ShowMedia/ShowMedia'
 import Banner from './components/Banner'
 import Welcome from './components/Welcome/Welcome'
+import Margin from './components/Margin'
+import Main from './components/Main'
 import { Routes, Route, Link } from "react-router-dom";
 import React from 'react';
 import './index.css';
 
-const styles = "text-red-700 text-xl font-bold underline " 
 
 function App() {
   return (
     <React.Fragment>
-      <div className="App">
-        <Nav />
-        <Banner />
-        <Routes>
-          <Route path="/welcome" element={<Welcome />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/showmedia" element={<ShowMedia />} />
-        </Routes>
-        <h1 className={styles}>
-          Hello world!
-        </h1>
+      <div className="flex flex-row h-screen">
+        <Margin />
+        <Main >
+          <Nav />
+          <Banner />
+          <Routes>
+            <Route path="/welcome" element={<Welcome />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/showmedia" element={<ShowMedia />} />
+          </Routes>
+        </Main>
+        <Margin />
       </div>
     </React.Fragment>
   );
