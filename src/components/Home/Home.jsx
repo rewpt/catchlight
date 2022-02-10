8ce2a556423d310600fae5235186d5323edd4db8
Marc Schulmeister <marcschulm@gmail.com>
2022-02-10 14:28:30 -0800

    clean up home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,23 +1,9 @@
 import { useEffect } from 'react';
 
 export default function Home() {
-  
-  function validateUser () {
-    const userToken = localStorage.getItem('userToken')
-    if (!userToken) {
-      // redirect to login
-    }
-    return userToken
-  };
-
-  useEffect(() => {
-    const token = validateUser();
-    alert(token);
-  }, []);
 
   return (
     <div>
-      Home 123z
     </div>
   )
 };
