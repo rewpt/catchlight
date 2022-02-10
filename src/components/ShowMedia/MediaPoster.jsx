428c7f68b9e067c528ae69c151d36f42a3e3cf71
rewpt <zpermack@gmail.com>
2022-02-10 18:12:00 -0400

    create content for media poster

--- src/components/ShowMedia/MediaPoster.jsx
+++ src/components/ShowMedia/MediaPoster.jsx
@@ -3,7 +3,11 @@ import * as React from 'react';
 export default function MediaPoster() {
   return (
   <React.Fragment>
-
+    <div className='w-3/4 h-full bg-cover bg-center row-span-2' >
+      <img className='h-full w-full object-cover' 
+        src='https://m.media-amazon.com/images/M/MV5BZmExNmEwYWItYmQzOS00YjA5LTk2MjktZjEyZDE1Y2QxNjA1XkEyXkFqcGdeQXVyMTQxNzMzNDI@.jpg' 
+      />
+    </div>
   </React.Fragment>
   )
 }
