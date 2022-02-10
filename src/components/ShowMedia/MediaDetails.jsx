2ad3069074e19b71a91a6a0c99a97cd239ceaa9d
rewpt <zpermack@gmail.com>
2022-02-10 15:28:22 -0400

    add grid to media details

--- src/components/ShowMedia/MediaDetails.jsx
+++ src/components/ShowMedia/MediaDetails.jsx
@@ -1,10 +1,19 @@
 import * as React from 'react';
+import RatingBar from './RatingBar'
+import RatingFaces from './RatingFaces';
 
 export default function MediaDetails() {
   return (
-   <div className='grid gap-y-3'>
-     <h1 className=' text-4xl font-extrabold'> Lord Of the Rings </h1>
+   <div className="grid grid-cols-2 gap-y-3">
+     <h1 className='media-title whitespace-nowrap text-3xl font-extrabold'> Lord Of the Rings </h1>
+     <div className='flex flex-row justify-center items-center'>
+     <RatingBar />
+     </div>
      <p> Young hobbit Frodo Baggins, after inheriting a mysterious ring from his uncle Bilbo, must leave his home in order to keep it from falling into the hands of its evil creator. Along the way, a fellowship is formed to protect the ringbearer and make sure that the ring arrives at its final destination: Mt. Doom, the only place where it can be destroyed.</p>
+     <RatingFaces />
+     
+     
+    
    </div>
   );
 }
