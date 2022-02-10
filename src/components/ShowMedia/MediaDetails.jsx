00752443bb72ce5cca9052906298ed8c741dd288
rewpt <zpermack@gmail.com>
2022-02-10 18:03:50 -0400

    Add new components to mediadetails

--- src/components/ShowMedia/MediaDetails.jsx
+++ src/components/ShowMedia/MediaDetails.jsx
@@ -1,17 +1,21 @@
 import * as React from 'react';
 import RatingBar from './RatingBar'
 import RatingFaces from './RatingFaces';
+import Title from './MediaTitle'
+import MediaPoster from './MediaPoster'
+import FriendInteractions from './FriendInteractions'
+import MediaDescription from './MediaDescription'
+import StreamsOn from './StreamsOn'
 
 export default function MediaDetails() {
   return (
-   <div className="grid grid-cols-2 gap-y-3">
-     <h1 className='media-title whitespace-nowrap text-3xl font-extrabold'> Lord Of the Rings </h1>
-     <div className='flex flex-row justify-center items-center'>
+   <div className="grid grid-cols-3 gap-y-3 mt-10 mx-10">
+     <MediaPoster />
+     <Title />
      <RatingBar />
-     </div>
-     <p className='font-semibold text-slate-800'> Young hobbit Frodo Baggins, after inheriting a mysterious ring from his uncle Bilbo, must leave his home in order to keep it from falling into the hands of its evil creator. Along the way, a fellowship is formed to protect the ringbearer and make sure that the ring arrives at its final destination: Mt. Doom, the only place where it can be destroyed.</p>
+     <FriendInteractions />
      <RatingFaces />
-     <h3>Watch on: Netflix, HBO</h3>
+     <StreamsOn />
      <div>
       <button className="btn">Watched!</button>
       <button className="btn">Add to Watch List</button>
