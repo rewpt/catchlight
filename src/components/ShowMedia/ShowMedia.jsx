4a6d8cbc102460178fe71f91881f6ed4546584d1
rewpt <zpermack@gmail.com>
2022-02-10 15:30:33 -0400

    make margin auto for showMedia

--- src/components/ShowMedia/ShowMedia.jsx
+++ src/components/ShowMedia/ShowMedia.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import MediaBanner from './MediaBanner'
 import MediaDetails from './MediaDetails'
+import Button from './MediaButton'
 
 export default function ShowMedia() {
   return (
     <React.Fragment>
-    <div className='w-4/6 m-auto'>
+    <div className='m-auto'>
       <MediaBanner />
       <MediaDetails />
     </div>
+    <Button>Add to Watched</Button>
+    <Button className='m-y-10'>Add to Watch List</Button>
     </React.Fragment>
   )
 }
