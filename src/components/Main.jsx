dcd66cc569a3c554cadf0dd0618555e7fda3011f
Brett Bilyk <brettbilyk@gmail.com>
2022-02-09 19:15:44 -0700

    Remove files

--- src/components/Main.jsx
+++
@@ -1,9 +0,0 @@
-export default function Main() {
-  return (
-    <div className="flex flex-row">
-      <div className="bg-cover bg-margin-bg-pattern bg-slate-500 bg-scroll">
-        <div className=" bg-slate-700 main-bg-pattern drop-shadow-2xl border-x-2 border-orange-300"></div>
-      </div>
-    </div>
-  );
-}
