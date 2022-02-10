618eb3f7dce443cfd2e4f2afc4286122647ecf75
Brett Bilyk <brettbilyk@gmail.com>
2022-02-10 15:12:38 -0700

    Change container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import './App.css';
 import Nav from './components/Nav';
-import Banner from './components/Banner'
+import SearchBox from './components/SearchBox'
 import { Outlet} from "react-router-dom";
 import React from 'react';
 import './index.css';
@@ -9,11 +9,10 @@ import './index.css';
 function App() {
   return (
     <React.Fragment>
-      
         <div className="flex h-screen bg-margin-pattern bg-slate-500 bg-scroll z-0">
-          <div className="container h-full mx-auto z-15 bg-white drop-shadow-2xl border-t-4 rounded-lg border-rose-400">
+          <div className="container h-full mx-auto z-15 bg-white drop-shadow-2xl border-t-4 rounded-2xl border-rose-500">
             <Nav />
-            <Banner />
+            <SearchBox />
             <Outlet />
           </div>
         </div>
