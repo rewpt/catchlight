ea8bd1712eb67edf9409891c464da03a9bdb980e
rewpt <zpermack@gmail.com>
2022-02-10 18:01:29 -0400

    Factored out StreamsOn to it's own component

---
+++ src/components/ShowMedia/StreamsOn.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function StreamsOn(){
+  return(
+    <React.Fragment>
+      <h3 className='font-semibold text-slate-800 mt-2'>Watch on: Netflix, HBO</h3>
+    </React.Fragment>
+  )
+}
