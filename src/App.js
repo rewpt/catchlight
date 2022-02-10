85dcd42c538b771e0ea38a465a6c82dab5fed7fb
Brett Bilyk <brettbilyk@gmail.com>
2022-02-10 08:13:39 -0700

    Move components

--- src/App.js
+++ src/App.js
@@ -9,10 +9,11 @@ import './index.css';
 function App() {
   return (
     <React.Fragment>
-      <Nav />
-      <Banner />
+      
         <div className="flex h-screen bg-margin-pattern bg-slate-500 bg-scroll z-0">
-          <div className="container h-full mx-auto z-15 bg-white drop-shadow-2xl border-t-4 rounded-lg border-rose-400 mt-11">
+          <div className="container h-full mx-auto z-15 bg-white drop-shadow-2xl border-t-4 rounded-lg border-rose-400">
+            <Nav />
+            <Banner />
             <Outlet />
           </div>
         </div>
