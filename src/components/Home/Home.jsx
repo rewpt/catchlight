6dbface96f1caa2b6f9220266e4fc011d4f62ee6
Brett Bilyk <brettbilyk@gmail.com>
2022-02-10 15:15:12 -0700

    Add CardContainers to Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,12 @@
+import React from "react"
+import CardContainer from "./CardContainer"
+
 export default function Home() {
   return (
-    <div>
-      Home
-    </div>
+    <React.Fragment>
+      <CardContainer>Friend Recommendations</CardContainer>
+      <CardContainer>Want to Watch</CardContainer>
+      <CardContainer>Watched</CardContainer>
+    </React.Fragment>
   )
 }
