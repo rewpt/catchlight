f7ec6beae7ff1d29247bb39aa83fab94e2ee6440
rewpt <zpermack@gmail.com>
2022-02-11 10:57:35 -0400

    Co-authored-by: Brett Bilyk <brettbilyk@gmail.com>

--- src/components/ShowMedia/RatingBar.jsx
+++ src/components/ShowMedia/RatingBar.jsx
@@ -1,12 +1,32 @@
 import * as React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFaceGrin, faFaceFrown, faFaceMeh } from '@fortawesome/free-solid-svg-icons'
 
 export default function RatingBar() {
   return (
-     <div className= 'flex h-6 max-w-sm min-w-full border border-black'>
-      <div className='like-bar w-1/3 h-full bg-green-500'>Like</div>
-      <div className='meh-bar w-1/3 h-full bg-yellow-500'>Meh</div>
-      <div className='dislike-bar w-1/3 h-full bg-red-500'>Dislike</div>
-     </div>
-     
+    <div className='ml-5 row-span-2 justify-items-center grid grid-cols-3'>
+     <div className='like-container'>
+       <div className='flex items-end h-full bg-gray-200 w-full rounded-full'>
+        <div className='h-1/2 rounded-b-full bg-green-200 w-full'></div>
+       </div>
+       <button><FontAwesomeIcon className='like-btn-clicked' icon={faFaceGrin} /> </button>
+      </div>
+      
+      <div className='meh-container'>
+       <div className='flex items-end h-full mr-2 bg-gray-200 rounded-full w-full'>
+        <div className='h-1/3 rounded-b-full bg-yellow-200 w-full'></div>
+       </div>
+       <button><FontAwesomeIcon className='meh-btn' icon={faFaceMeh} /> </button>
+      </div>
+
+      <div className='dislike-container'>
+        <div className='flex items-end h-full bg-gray-200 rounded-full w-full'>
+          <div className='h-1/3 rounded-b-full bg-red-200 w-full'></div>
+        </div>
+        <button><FontAwesomeIcon className='dislike-btn' icon={faFaceFrown} /> </button>
+       </div>
+
+        
+    </div>
   );
 }
