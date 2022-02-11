44615b9a0ce83118f77dcf0ecfc16106424f0c2e
rewpt <zpermack@gmail.com>
2022-02-11 10:58:15 -0400

    change styling of bars and combined with rating faces

--- src/components/ShowMedia/MediaTitle.jsx
+++ src/components/ShowMedia/MediaTitle.jsx
@@ -3,8 +3,8 @@ import MediaDescription from './MediaDescription'
 
 export default function MediaTitle() {
   return (
-    <div className='flex-column'>
-      <h1 className='media-title whitespace-nowrap text-3xl font-extrabold'> The Matrix </h1>
+    <div className=''>
+      <h1 className='media-title whitespace-nowrap text-5xl font-extrabold'> The Matrix </h1>
       <MediaDescription />
     </div>
   );
--- src/components/ShowMedia/RatingFaces.jsx
+++ src/components/ShowMedia/RatingFaces.jsx
@@ -5,10 +5,10 @@ import { faFaceGrin, faFaceFrown, faFaceMeh } from '@fortawesome/free-solid-svg-
 export default function RatingFaces() {
 
   return (
-      <div>
-        <button><FontAwesomeIcon className='text-2xl mr-2 text-slate-400 hover:text-amber-500 cursor-pointer' icon={faFaceGrin} /> </button>
-        <button><FontAwesomeIcon className='text-2xl mr-2 text-slate-400 hover:text-amber-500' icon={faFaceMeh} /> </button>
-        <button><FontAwesomeIcon className='text-2xl text-slate-400 hover:text-amber-500' icon={faFaceFrown} /> </button>
+      <div className='flex justify-center'>
+        <button><FontAwesomeIcon className='text-7xl mr-2 text-slate-400 hover:text-green-500 cursor-pointer' icon={faFaceGrin} /> </button>
+        <button><FontAwesomeIcon className='text-7xl mr-2 text-slate-400 hover:text-amber-500' icon={faFaceMeh} /> </button>
+        <button><FontAwesomeIcon className='text-7xl text-slate-400 hover:text-red-500' icon={faFaceFrown} /> </button>
       </div>
   )
 }
