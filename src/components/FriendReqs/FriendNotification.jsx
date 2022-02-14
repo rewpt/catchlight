b610335c83c8198a549369c47496e5cc1e61a7b0
rewpt <zpermack@gmail.com>
2022-02-14 17:04:29 -0400

    create styling and ajax calls  for friendnotifications

---
+++ src/components/FriendReqs/FriendNotification.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import axios from "axios";
+
+export default function FriendNotification() {
+  const getFriendReqs = async (e) => {
+    e.preventDefault();
+    try {
+      const token = localStorage.getItem("userToken");
+      const response = await axios.get(
+        "http://localhost:3001/api/friends/requests",
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+      console.log(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const token = localStorage.getItem("userToken");
+  return (
+    <div className="text-lg bg-white border-black border-1 border-solid z-50 overflow-hidden  w-[400px] absolute top-[64px] right-0">
+      <div className="grid auto-cols-max grid-cols-3">
+        <img
+          className="inline-block h-10 w-10 rounded-full ring-2 ring-white z-20  ml-2 mt-3"
+          src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+          alt=""
+        />
+        <span className="w-full h-full">
+          Brett (brett@gmail.com) wants to be your friend
+        </span>
+        <button onClick={getFriendReqs} className="btn">
+          Add Friend
+        </button>
+      </div>
+    </div>
+  );
+}
