deacac5efe9e13f0c0b9c51e76b96a40207b79a4
Marc Schulmeister <marcschulm@gmail.com>
2022-02-15 10:21:54 -0800

    move getMedia inside useEffect

--- src/components/Home/FriendRecommendations.jsx
+++ src/components/Home/FriendRecommendations.jsx
@@ -7,26 +7,27 @@ export default function FriendRecommendations(props) {
 
   const [mediaData, setMediaData] = useState([]);
   
-  async function getMedia() {
-    try {
-      const media = await axios.get('/api/mediaFriendsRecommendations', {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('userToken')}`
-        }
-      });
-      // console.log(media.data);
-      return media.data;
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
   useEffect(() => {
+
+    async function getMedia() {
+      try {
+        const media = await axios.get('/api/mediaFriendsRecommendations', {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('userToken')}`
+          }
+        });
+        // console.log(media.data);
+        return media.data;
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
     getMedia().then((res) => {
       setMediaData(res);
-    })
+    });
   }, []);
-
+  
   return (
     <div>
       <CardContainer
