597b52762497241f60dbd12bf7cc8ac582f95569
Brett Bilyk <brettbilyk@gmail.com>
2022-02-15 16:56:20 -0700

    Change to relative and bg-white.  Implement skele

--- src/components/ChatBox/ChatContent.jsx
+++ src/components/ChatBox/ChatContent.jsx
@@ -1,11 +1,16 @@
+import FriendTabGroup from "./FriendTabGroup";
+import FriendConversations from "./FriendConversations"
+import { useState } from "react"
 
 export default function ChatContent() {
 
+  const [ activeTab, setActiveTab ] = useState()
 
 
   return (
-  <div className="static bg-white w-full">
-
+  <div className="relative bg-white w-full">
+    <FriendTabGroup activeTab={activeTab} onClick={setActiveTab}/>
+    <FriendConversations />
   </div>
   );
 }
