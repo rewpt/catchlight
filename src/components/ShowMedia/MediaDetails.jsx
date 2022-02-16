8233219518a3406c832a296d90bf3c98c5e8ccb1
Brett Bilyk <brettbilyk@gmail.com>
2022-02-16 13:49:57 -0700

    Change from hardcoded to axios request values

--- src/components/ShowMedia/MediaDetails.jsx
+++ src/components/ShowMedia/MediaDetails.jsx
@@ -1,17 +1,46 @@
-import * as React from 'react';
-import RatingBar from './RatingBar'
+import {React, useEffect, useState} from 'react';
+import { useParams } from "react-router-dom";
+import RatingBar from './RatingBar';
 import RatingFaces from './RatingFaces';
-import Title from './MediaTitle'
+import Title from './MediaTitle';
 import MediaPoster from './MediaPoster'
-import FriendInteractions from './FriendInteractions'
-import MediaDescription from './MediaDescription'
-import StreamsOn from './StreamsOn'
+import FriendInteractions from './FriendInteractions';
+import MediaDescription from './MediaDescription';
+import StreamsOn from './StreamsOn';
+import axios from 'axios';
 
 export default function MediaDetails() {
+  const { id } = useParams()
+  const [mediaDetails, setMediaDetails] = useState({
+    image: null,
+    title: null,
+    description: null
+  }
+  )
+  console.log(mediaDetails)
+  useEffect(() => {
+    async function getSingleMedia() {
+
+      try {
+        const singleMedia = await axios.get(`/api/media/${id}`, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('userToken')}`
+          }
+        });
+        return setMediaDetails(singleMedia.data)
+
+      } catch(err) {
+        console.error(err)
+      }
+    }
+
+    return getSingleMedia()
+  }, [id]);
+
   return (
    <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-6 gap-x-6 mt-10 mx-10">
-     <MediaPoster />
-     <Title />
+     <MediaPoster image={mediaDetails.image}/>
+     <Title title={mediaDetails.title} description={mediaDetails.description}/>
      <RatingBar />
      <FriendInteractions />
      <StreamsOn />
@@ -19,9 +48,6 @@ export default function MediaDetails() {
       <button className="btn-clicked mr-1">Watched!</button>
       <button className="btn ml-1">Add to Watch List</button>
     </div>
-     
-     
-    
    </div>
   );
 }
--- src/components/ShowMedia/MediaTitle.jsx
+++ src/components/ShowMedia/MediaTitle.jsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import MediaDescription from './MediaDescription'
 
-export default function MediaTitle() {
+export default function MediaTitle(props) {
+  const { title, description } = props;
+
   return (
     <div className=''>
-      <h1 className='media-title whitespace-nowrap text-5xl font-extrabold'> The Matrix </h1>
-      <MediaDescription />
+      <h1 className='media-title whitespace-nowrap text-5xl font-extrabold'>{title}</h1>
+      <MediaDescription description={description}/>
     </div>
   );
 }
