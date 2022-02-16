51ee7af967bd0460cdc1c49672b0168c8447c377
Brett Bilyk <brettbilyk@gmail.com>
2022-02-15 18:42:31 -0700

    Change height and margin

--- src/components/ChatBox/FriendTab.jsx
+++ src/components/ChatBox/FriendTab.jsx
@@ -4,7 +4,7 @@ export default function FriendTab(props) {
 
 
   return (
-  <div className="flex justify-center mb-1.5 h-32 border-b-4 border-rose-500">
+  <div className="flex justify-center mb-2 h-24 border-b-4 border-rose-500">
     <img className='max-w-none hover:opacity-50' src={avatarUrl} alt="Your Profile Avatar"/>
   </div>
   );
