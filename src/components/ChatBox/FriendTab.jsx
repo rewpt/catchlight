90f6622e88c11fd1e233b96976049ac3a3f66f7a
Brett Bilyk <brettbilyk@gmail.com>
2022-02-15 17:08:19 -0700

    Update hover feedback

--- src/components/ChatBox/FriendTab.jsx
+++ src/components/ChatBox/FriendTab.jsx
@@ -4,8 +4,8 @@ export default function FriendTab(props) {
 
 
   return (
-  <div className="relative flex justify-center mb-1.5 h-32 bg-gray-200 border-b-4 border-rose-500">
-    <img className='h-full max-w-none' src={avatarUrl} alt="Your Profile Avatar"/>
+  <div className="flex justify-center mb-1.5 h-32 border-b-4 border-rose-500">
+    <img className='max-w-none hover:opacity-50' src={avatarUrl} alt="Your Profile Avatar"/>
   </div>
   );
 }
