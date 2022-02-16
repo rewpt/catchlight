1dae9f77f0e0f81804e6d67129c7796efd196b16
Brett Bilyk <brettbilyk@gmail.com>
2022-02-16 13:48:59 -0700

    Add react router Link

--- src/components/Home/CardItem.jsx
+++ src/components/Home/CardItem.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import BottomPop from "./BottomPop";
+import { Link } from 'react-router-dom'
 
 export default function CardItem(props) {
-  const [hidden, setHidden] = useState(true);
+  const [ hidden, setHidden ] = useState(true);
 
   return (
     <div
@@ -14,8 +15,10 @@ export default function CardItem(props) {
       }}
       className="flex flex-col mt-10 min-w-[150px] min-h-[220px] max-w-[150px] max-h-[220px] mx-4 hover:scale-120 ease-in duration-100 origin-bottom"
     >
-      <img className="h-full w-full" src={props.image} alt="" />
-      {!hidden && <BottomPop />}
+      <Link to={`/media/${props.id}`} key={props.id}>
+        <img className="h-full w-full" src={props.image} alt="" />
+        {!hidden && <BottomPop />}
+      </Link>
     </div>
   );
 }
