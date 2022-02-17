8975cafeb52375b568d3fc8f9b9db29ac0940c82
Brett Bilyk <brettbilyk@gmail.com>
2022-02-17 12:05:45 -0700

    Add logic for adaptive MediaDetails

--- src/components/ShowMedia/MediaDetails.jsx
+++ src/components/ShowMedia/MediaDetails.jsx
@@ -1,27 +1,55 @@
-import * as React from 'react';
-import RatingBar from './RatingBar'
-import RatingFaces from './RatingFaces';
-import Title from './MediaTitle'
+import {React, useEffect, useState} from 'react';
+import { useParams } from "react-router-dom";
+import RatingBar from './RatingBar';
+import Title from './MediaTitle';
 import MediaPoster from './MediaPoster'
-import FriendInteractions from './FriendInteractions'
-import MediaDescription from './MediaDescription'
-import StreamsOn from './StreamsOn'
+import FriendInteractions from './FriendInteractions';
+import MediaWatchedButton from './MediaWatchedButton';
+import StreamsOn from './StreamsOn';
+import axios from 'axios';
 
 export default function MediaDetails() {
+  const { id } = useParams()
+  const [ mediaInteraction, setMediaInteraction] = useState({})
+  const [ mediaDetails, setMediaDetails] = useState({});
+  const [ buttonText, setButtonText ] = useState("")
+
+  useEffect(() => {
+    const jwt = {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('userToken')}`
+      }
+    }
+    
+    const singleMedia = axios.get(`/api/media/${id}`, jwt);
+    const userInteraction = axios.get(`/api/media/${id}/interactions/`, jwt);
+
+    Promise.all([singleMedia, userInteraction])
+      .then(([media, interaction]) => {
+        setMediaDetails(media.data)
+        setMediaInteraction(interaction.data)
+
+        if(interaction.data.rating === "interest") {
+          setButtonText("Remove from Watch List")
+        } else if(interaction.data.rating === undefined) {
+          setButtonText("Add to Watch List")
+        } else {
+          // i'm tired and don't know how to indicate to the user that a rating can be removed so i'm leaving this here for now
+          setButtonText("Remove your Rating")
+        }
+      })
+  }, [id]);
+
   return (
    <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-6 gap-x-6 mt-10 mx-10">
-     <MediaPoster />
-     <Title />
-     <RatingBar />
+     <MediaPoster image={mediaDetails.image}/>
+     <Title title={mediaDetails.title} description={mediaDetails.description}/>
+     <RatingBar mediaInteraction={mediaInteraction}/>
      <FriendInteractions />
      <StreamsOn />
      <div className=' flex'>
-      <button className="btn-clicked mr-1">Watched!</button>
-      <button className="btn ml-1">Add to Watch List</button>
+     <MediaWatchedButton>{buttonText}</MediaWatchedButton>
     </div>
-     
-     
-    
    </div>
   );
 }
