0ba1d4713e66d88f54676e920db0b56453b9459d
Brett Bilyk <brettbilyk@gmail.com>
2022-02-17 12:06:00 -0700

    Add props for media image

--- src/components/ShowMedia/MediaPoster.jsx
+++ src/components/ShowMedia/MediaPoster.jsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 
-export default function MediaPoster() {
+export default function MediaPoster(props) {
+  const { image } = props
+
   return (
   <React.Fragment>
     <div className='flex justify-center lg:justify-start w-full row-span-2'>
       <div className='w-full border-black border-solid drop-shadow-md border-2 h-full bg-cover bg-center ' >
         <img className='h-full w-full object-cover' 
-          src='https://m.media-amazon.com/images/M/MV5BZmExNmEwYWItYmQzOS00YjA5LTk2MjktZjEyZDE1Y2QxNjA1XkEyXkFqcGdeQXVyMTQxNzMzNDI@.jpg' 
+          src={image} 
         />
       </div>
     </div>
