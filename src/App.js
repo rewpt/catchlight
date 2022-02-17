806463b829e75317e9cd15a3205860ec37ec9877
rewpt <zpermack@gmail.com>
2022-02-17 12:47:30 -0400

    Change min-height to show full page for media

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <React.Fragment>
-        <div className="absolute flex content-center w-screen bg-gray-200 ">
+        <div className="absolute flex min-h-[100%] content-center w-screen bg-gray-200 ">
           <div className="relative w-full xs:w-full lg:w-[1050px] xl:w-[1200px] mx-auto bg-white drop-shadow-2xl border-t-4 border-rose-500">
             <Nav />
             <SearchBox />
