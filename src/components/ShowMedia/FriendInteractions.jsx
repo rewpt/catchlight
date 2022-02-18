250b7e69ae9a0751cd26a891c266147d27d84a83
Brett Bilyk <brettbilyk@gmail.com>
2022-02-18 13:14:11 -0700

    Update avatars with live data

--- src/components/ShowMedia/FriendInteractions.jsx
+++ src/components/ShowMedia/FriendInteractions.jsx
@@ -1,28 +1,21 @@
 import React from 'react'
 
-export default function FriendInteractions() {
+export default function FriendInteractions(props) {
+  const { friendsAvatarArray } = props;
+  console.log("🚀 ~ file: FriendInteractions.jsx ~ line 5 ~ FriendInteractions ~ friendsAvatarArray", friendsAvatarArray)
+
+
   return (
     <div className="flex xs:justify-center sm:justify-center lg:justify-start -space-x-3">
-    <img
-      className="media-avatar"
-      src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-      alt=""
-    />
-    <img
-      className="media-avatar"
-      src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-      alt=""
-    />
-    <img
-      className="media-avatar"
-      src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
-      alt=""
-    />
-    <img
-      className="media-avatar"
-      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-      alt=""
-    />
+    {friendsAvatarArray.map((image, index) => {
+        
+        return <img
+          key={index}
+          className="media-avatar"
+          src={image.profile_picture}
+          alt=""
+        />
+      })}
   </div>
   );
 }
--- src/components/ShowMedia/MediaDetails.jsx
+++ src/components/ShowMedia/MediaDetails.jsx
@@ -1,4 +1,4 @@
-import {React, useEffect, useState} from 'react';
+import { React, useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
 import RatingBar from './RatingBar';
 import Title from './MediaTitle';
@@ -12,7 +12,12 @@ export default function MediaDetails() {
   const { id } = useParams()
   const [ mediaInteraction, setMediaInteraction] = useState({})
   const [ mediaDetails, setMediaDetails] = useState({});
-
+  const [ dislikePercent, setDislikePercent ] = useState({});
+  const [ likePercent, setLikePercent ] = useState({});
+  const [ mehPercent, setMehPercent ] = useState({});
+  const [ friendsAvatars, setFriendsAvatars ] = useState([]);
+  
+  
   useEffect(() => {
     const jwt = {
       headers: {
@@ -22,20 +27,44 @@ export default function MediaDetails() {
     
     const singleMedia = axios.get(`/api/media/${id}`, jwt);
     const userInteraction = axios.get(`/api/media/${id}/interactions/`, jwt);
+    const totalUsersInteractions = axios.get(`/api/interactions/count/${id}`, jwt);
+    const friendsPictures = axios.get('/api/friendsPictures', jwt);
+    const mediaFriendsInteractions = axios.get('/api/mediaFriendsRecommendations', jwt);
+    
+    Promise.all([singleMedia, userInteraction, totalUsersInteractions, friendsPictures, mediaFriendsInteractions])
+    .then(([media, interaction, percent, friendsPictures, mediaFriendsInteractions]) => {
+      setMediaDetails(media.data);
+      setMediaInteraction(interaction.data);
+      setLikePercent((percent.data[0].like_count / percent.data[0].total_count) * 100);
+      setDislikePercent((percent.data[0].dislike_count / percent.data[0].total_count) * 100);
+      setMehPercent((percent.data[0].meh_count / percent.data[0].total_count) * 100);
 
-    Promise.all([singleMedia, userInteraction])
-      .then(([media, interaction]) => {
-        setMediaDetails(media.data)
-        setMediaInteraction(interaction.data)
-      })
+      const results = []
+
+      for (const friend of friendsPictures.data) {
+        for (const mediaFriend of mediaFriendsInteractions.data) {
+          if (friend.friend_id === mediaFriend.id) {
+            for (const interactionMedia of mediaFriend.interactions) {
+              if (interactionMedia.media_id === ~~id) {
+                results.push({profile_picture: friend.profile_picture});
+              }
+            }
+          }
+        }
+      }
+      setFriendsAvatars(results);
+    })
+    .catch((error) =>  {
+      console.error(error)
+    });
   }, [id]);
-
+  
   return (
    <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-6 gap-x-6 mt-10 mx-10">
      <MediaPoster image={mediaDetails.image}/>
      <Title title={mediaDetails.title} description={mediaDetails.description}/>
-     <RatingBar mediaInteraction={mediaInteraction}/>
-     <FriendInteractions />
+     <RatingBar dislikePercent={dislikePercent} likePercent={likePercent} mehPercent={mehPercent} mediaInteraction={mediaInteraction}/>
+     <FriendInteractions friendsAvatarArray={friendsAvatars}/>
      <StreamsOn />
      <div className=' flex'>
      {mediaInteraction.rating === "interest" && <MediaWatchedButton>Remove from Watch List</MediaWatchedButton>}
