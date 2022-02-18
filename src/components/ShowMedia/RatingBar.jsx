5ea8a323056bafb28ce33d06816adb41c4e47523
Brett Bilyk <brettbilyk@gmail.com>
2022-02-18 13:14:24 -0700

    Update rating bar with live data

--- src/components/ShowMedia/RatingBar.jsx
+++ src/components/ShowMedia/RatingBar.jsx
@@ -4,36 +4,34 @@ import { faFaceGrin, faFaceFrown, faFaceMeh } from '@fortawesome/free-solid-svg-
 import classNames from "classnames";
 
 export default function RatingBar(props) {
-  const { mediaInteraction } = props;
+  const { dislikePercent, likePercent, mehPercent, mediaInteraction } = props;
+  const [dummyState, setDummyState] = React.useState(33)
+    const toggleLike = classNames({"like-btn-clicked": mediaInteraction.rating === "like", "like-btn": mediaInteraction !== "like"});
+    const toggleMeh = classNames({"meh-btn-clicked": mediaInteraction.rating === "meh", "meh-btn": mediaInteraction !== "meh"});
+    const toggleDislike = classNames({"dislike-btn-clicked": mediaInteraction.rating === "dislike", "dislike-btn": mediaInteraction !== "dislike"});
 
-  const toggleLike = classNames({"like-btn-clicked": mediaInteraction.rating === "like", "like-btn": mediaInteraction !== "like"});
-  const toggleMeh = classNames({"meh-btn-clicked": mediaInteraction.rating === "meh", "meh-btn": mediaInteraction !== "meh"});
-  const toggleDislike = classNames({"dislike-btn-clicked": mediaInteraction.rating === "dislike", "dislike-btn": mediaInteraction !== "dislike"});
 
   return (
     <div className='row-span-2 justify-items-center grid grid-cols-3'>
      <div className='like-container'>
        <div className='flex items-end h-full bg-gray-200 w-full rounded-full'>
-        <div className='h-1/2 rounded-b-full bg-green-200 w-full'></div>
-       </div>
+        <div className='rounded-b-full bg-green-200 w-full' style={{height: `${likePercent}%`}}></div>  
+        </div>
        <button><FontAwesomeIcon className={toggleLike} icon={faFaceGrin} /> </button>
       </div>
       
       <div className='meh-container'>
        <div className='flex items-end h-full mr-2 bg-gray-200 rounded-full w-full'>
-        <div className='h-1/3 rounded-b-full bg-yellow-200 w-full'></div>
+        <div className={`rounded-b-full bg-yellow-200 w-full`} style={{height: `${mehPercent}%`}}></div>
        </div>
        <button><FontAwesomeIcon className={toggleMeh} icon={faFaceMeh} /> </button>
       </div>
-
       <div className='dislike-container'>
         <div className='flex items-end h-full bg-gray-200 rounded-full w-full'>
-          <div className='h-1/3 rounded-b-full bg-red-200 w-full'></div>
+          <div className={'rounded-b-full bg-red-200 w-full'} style={{height: `${dislikePercent}%`}}></div>
         </div>
         <button><FontAwesomeIcon className={toggleDislike} icon={faFaceFrown} /> </button>
-       </div>
-
-        
+      </div>
     </div>
   );
 }
