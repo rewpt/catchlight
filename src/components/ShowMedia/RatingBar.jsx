7bcb5bbede33cf6033e229c39f7f17141bb1b1bc
Brett Bilyk <brettbilyk@gmail.com>
2022-02-19 22:09:26 -0700

    Fix css bug when bar at 100%

--- src/components/ShowMedia/RatingBar.jsx
+++ src/components/ShowMedia/RatingBar.jsx
@@ -9,7 +9,9 @@ export default function RatingBar(props) {
   const { like_count, dislike_count, meh_count, total_count } = interactionStats
 
   function getPercentages(count) {
-    return `${(parseInt(count) / parseInt(total_count)) * 100}%`
+    // return {} to empty bars if percent === 0
+    if(total_count === 0) return {};
+    return {height: `${(parseInt(count) / parseInt(total_count)) * 100}%`}
   }
 
   const toggleLike = classNames({"like-btn-clicked": mediaInteraction.rating === "like", "like-btn": mediaInteraction.rating !== "like"});
@@ -26,20 +28,20 @@ export default function RatingBar(props) {
     <div className='row-span-2 justify-items-center grid grid-cols-3'>
      <div className='like-container'>
        <div className='flex items-end h-full bg-gray-200 w-full rounded-full'>
-        <div className='rounded-b-full bg-green-200 w-full' style={{height: getPercentages(like_count)}}></div>  
+        <div className='rounded-b-full rounded-t-full bg-green-200 w-full' style={getPercentages(like_count)}></div>  
         </div>
        <button><FontAwesomeIcon onClick={() => {handleRatingClick('like');}} className={toggleLike} icon={faFaceGrin} /> </button>
       </div>
       
       <div className='meh-container'>
        <div className='flex items-end h-full mr-2 bg-gray-200 rounded-full w-full'>
-        <div className={`rounded-b-full bg-yellow-200 w-full`} style={{height: getPercentages(meh_count)}}></div>
+        <div className={`rounded-b-full rounded-t-full bg-yellow-200 w-full`} style={getPercentages(meh_count)}></div>
        </div>
        <button><FontAwesomeIcon onClick={() => {handleRatingClick('meh');}} className={toggleMeh} icon={faFaceMeh} /> </button>
       </div>
       <div className='dislike-container'>
         <div className='flex items-end h-full bg-gray-200 rounded-full w-full'>
-          <div className={'rounded-b-full bg-red-200 w-full'} style={{height: getPercentages(dislike_count)}}></div>
+          <div className={'rounded-b-full rounded-t-full bg-red-200 w-full'} style={getPercentages(dislike_count)}></div>
         </div>
         <button><FontAwesomeIcon onClick={() => {handleRatingClick('dislike');}} className={toggleDislike} icon={faFaceFrown} /> </button>
       </div>
