e16e5b409e76eaf3d4136adc0bebaa8e3026f1e9
Brett Bilyk <brettbilyk@gmail.com>
2022-02-20 16:24:36 -0700

    Add logic for loading data

--- src/components/ShowMedia/MediaDetails.jsx
+++ src/components/ShowMedia/MediaDetails.jsx
@@ -1,4 +1,4 @@
-import { React } from 'react';
+import React from 'react';
 import { useApplicationData } from '../../hooks/useApplicationData';
 import RatingBar from './RatingBar';
 import Title from './MediaTitle';
@@ -21,21 +21,30 @@ export default function MediaDetails() {
     buttonState, 
     setButtonState,
     postMediaButtonClick,
-    handleRatingClick
+    handleRatingClick,
+    isLoading
   } = useApplicationData()
 
-  //---------------- axios calls and data manipulation --------------------
-
   return (
-   <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-6 gap-x-6 mt-10 mx-10">
-     <MediaPoster image={mediaDetails.image}/>
-     <Title title={mediaDetails.title} description={mediaDetails.description}/>
-     <RatingBar handleRatingClick={handleRatingClick} interactionStats={interactionStats} mediaInteraction={mediaInteraction} />
-     <FriendInteractions friendsAvatarArray={friendsAvatars}/>
-     <StreamsOn streamingServices={streamingServices}/>
-     <div className=' flex'>
-     <MediaWatchedButton mediaButtonClick={postMediaButtonClick} buttonState={buttonState} setButtonState={setButtonState} handleRatingClick={handleRatingClick}></MediaWatchedButton>
-    </div>
-   </div>
+    <React.Fragment>
+      { !isLoading && (
+     <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-6 gap-x-6 mt-10 mx-10">
+       <MediaPoster image={mediaDetails.image}/>
+       <Title title={mediaDetails.title} description={mediaDetails.description}/>
+       <RatingBar handleRatingClick={handleRatingClick} interactionStats={interactionStats} mediaInteraction={mediaInteraction} />
+       <FriendInteractions friendsAvatarArray={friendsAvatars}/>
+       <StreamsOn streamingServices={streamingServices}/>
+       <div className=' flex'>
+       <MediaWatchedButton postMediaButtonClick={postMediaButtonClick} buttonState={buttonState} setButtonState={setButtonState} handleRatingClick={handleRatingClick}></MediaWatchedButton>
+      </div>
+     </div>
+    )}
+    {isLoading && (
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-6 gap-x-6 mt-10 mx-10">
+      Loading...
+      </div>
+    )}
+
+    </React.Fragment>
   );
 }
