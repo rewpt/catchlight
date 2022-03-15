384f9b85b1678087c5db7868439f4c1e804d2c52
rewpt <zpermack@gmail.com>
2022-03-15 13:57:54 -0400

    make default route registration

--- src/helpers/ProtectedRoutes.jsx
+++ src/helpers/ProtectedRoutes.jsx
@@ -2,5 +2,5 @@ import { Navigate, Outlet } from "react-router";
 
 export default function ProtectedRoutes() {
   const token = localStorage.getItem("userToken");
-  return token ? <Outlet /> : <Navigate to="/login" />;
+  return token ? <Outlet /> : <Navigate to="/register" />;
 }
